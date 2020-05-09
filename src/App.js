844b43be9e1d7d89c0332aee2fdf6f67cf29294d
Md Muidul Alam <md.muidul.alam@gmail.com>
2020-05-09 15:53:00 +0600

    IMS-17: Implement first component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Singin from "./components/backboneComponents/account/signin";
 
 function App() {
   return (
     <div className="App">
-      
+      <Singin />
     </div>
   );
 }
