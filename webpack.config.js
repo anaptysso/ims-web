0cf2cf7f0dfc1ded65b1e4eb2f5650374c02aa72
Md Muidul Alam <md.muidul.alam@gmail.com>
2020-06-14 17:48:57 +0600

    IMS-20: Configure redux devtool

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,37 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { dirname } = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'index_bundle.js'
-    },
-    devServer: {
-        port: 3000,
-        host: 'localhost',
-        //Be possible go back pressing the "back" button at chrome
-        historyApiFallback: true
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js",
+  },
+  devServer: {
+    port: 3000,
+    host: "localhost",
+    //Be possible go back pressing the "back" button at chrome
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
       },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            { 
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'React App',
-            template: './public/index.html'
-        })
-    ]
-}
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "React App",
+      template: "./public/index.html",
+    }),
+  ],
+};
