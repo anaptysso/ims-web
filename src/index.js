63690bb08a7570c2a3064e285c3ffffebc040276
Md Muidul Alam <md.muidul.alam@gmail.com>
2020-07-14 08:19:30 +0600

    IMS-33: Configure less in the project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./index.css";
+import "./index.less";
 import App from "./App.jsx";
 import * as serviceWorker from "./serviceWorker";
 
